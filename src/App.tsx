707ad49d53b7f5dfb1106188b3bf36eb0ac4a8c3
Davide Baldi <d.baldi88@gmail.com>
2024-07-10 10:43:13 +0200

    save before gym

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
+import { ReactNode, useState } from "react";
 import CourseGoal from "./components/CourseGoal.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "/goals.jpg"
 
 
+type CourseGoal = {
+  title: string,
+  description: ReactNode,
+  id: number
+}
+
+
 export default function App() {
+
+    const [goal, setGoal] = useState<CourseGoal[]>([])
+
+
+  const handleGoal = () => {
+    setGoal((prevGoals)=> {
+      const newGoal: CourseGoal = {
+        title: "hello",
+        description: "banan",
+        id: 3
+      }
+      return [...prevGoals, newGoal ]
+
+    })
+  }
+
+
   return (
     <>
     <Header src={goalsImg} alt={"image"}  >
         your course goal
     </Header>
+    <button onClick={handleGoal}>Add goal</button>
     <CourseGoal title="Learn TS+ React" children="an amazing course!!!"/>
+    <ul>
+    {goal.map((item, index)=>
     
-    
+      <li index={item.id}>
+            <CourseGoal children={undefined} {...item}/>
+
+
+      </li>
+      
+      )}
+    </ul>
     </>
   ) 
 }
---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,11 @@
+
+
+const CourseGoalList = () => {
+    return (
+        <div>
+          
+        </div>
+    )
+};
+
+export default CourseGoalList
